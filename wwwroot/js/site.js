72cf7eb71237d4ab9857d8f02f97bf670dfb1a8b
rpgcunha <rpgcunha@hotmail.com>
2023-03-24 09:14:06 +0000

    retirar o home ative

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿for (var i = 0; i < document.links.length; i++) {
-    if (document.links[i].href == document.URL) {
+    if (document.links[i].href == document.URL && document.links[i] != "http://localhost:5268/") {
         document.links[i].className = 'linkAtivo';
     }
 }
