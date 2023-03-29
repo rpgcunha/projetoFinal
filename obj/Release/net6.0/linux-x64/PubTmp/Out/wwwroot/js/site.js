48ab29403d2cb18a93af6c92db4dea5711394527
rpgcunha <rpgcunha@hotmail.com>
2023-03-29 17:05:45 +0100

    publish

---
+++ obj/Release/net6.0/linux-x64/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿var navbar = document.getElementById("myNavbar");
+var links = navbar.getElementsByTagName("a");
+
+for (var i = 0; i < links.length; i++) {
+    if (links[i].href == document.URL && links[i] != "http://localhost:5268/") {
+        links[i].className = "linkAtivo";
+    }
+}
+
+const passwordSpans = document.querySelectorAll('.password');
+
+passwordSpans.forEach(function (passwordSpan) {
+    const passwordText = passwordSpan.getAttribute('data-password');
+    passwordSpan.addEventListener('mouseover', function () {
+        passwordSpan.textContent = passwordText;
+    });
+    passwordSpan.addEventListener('mouseout', function () {
+        passwordSpan.textContent = '********';
+    });
+});
